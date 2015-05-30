e636ba0b1e09ddc8850c29d61433812b8344c5da
Scentle5S <nicolas.bfd@gmail.com>
2015-05-30 16:59:13 +0200

    Remet les sémaphores

--- src/project.c
+++ src/project.c
@@ -5,7 +5,16 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/sem.h> 
 #include <errno.h>
+
+#define MUTEX 21
+
+typedef struct Voie {
+	int num;
+	int sem_num[6];
+	int crois[6];
+} Voie;
 
 typedef struct Voiture {
 	int numero;
@@ -25,6 +34,58 @@ typedef struct {
 } treponse;
 
 int msgid;
+int sem_id;
+struct sembuf sem_oper ;  /* Operation P & V */
+
+Voie voies[12] = {	{1,{0,1,2,3,4,5},{11,7,10,4,7,8}},
+							{2,{6,7,8,9},{11,7,4,5}},
+							{3,{-1},{-1}},
+							{4,{8,10,3,11,12,13},{2,10,1,7,10,11}},
+							{5,{9,14,15,16},{2,10,7,8}},
+							{6,{-1},{-1}},
+							{7,{15,4,11,17,1,7},{5,1,4,10,1,2}},
+							{8,{16,5,18,19},{5,1,10,11}},
+							{9,{-1},{-1}},
+							{10,{18,12,17,2,10,14},{8,4,7,1,4,5}},
+							{11,{19,13,0,6},{8,4,1,2}},
+							{12,{-1},{-1}}	};
+							
+void P(int semnum)
+{
+	//printf("Blocage semaphore %d\n",semnum);
+	sem_oper.sem_num = semnum-1;	// Numéro du sémaphore
+	sem_oper.sem_op = -1 ;	// Opération (P(s))
+	sem_oper.sem_flg = 0 ;	// NULL
+
+	semop(sem_id,&sem_oper,1);
+}
+
+void V(int semnum)
+{
+	//printf("Deblocage semaphore %d\n",semnum);
+	sem_oper.sem_num = semnum-1;	// Numéro du sémaphore
+	sem_oper.sem_op = 1 ;	// Opération (V(s))
+	sem_oper.sem_flg = 0 ;	// NULL
+
+	semop(sem_id,&sem_oper,1);
+}
+
+int initsem() 
+{
+   sem_id = semget(IPC_PRIVATE, 16, IPC_CREAT | IPC_EXCL | 0666);
+    
+   union semun {
+			int val;
+			struct semid_ds *stat;
+			ushort *array;
+	} ctl_arg;
+
+	ushort array[21] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};	// 20 => MUTEX
+	ctl_arg.array = array;
+	semctl(sem_id, 0, SETALL, ctl_arg);
+
+	return (sem_id);
+}
 
 void initRand()
 {
@@ -93,7 +154,7 @@ void serveur()
 	treponse rep;
 	int  tailleReq,tailleRep;
 
-	tailleReq    = sizeof(trequete) - sizeof(long);
+	tailleReq = sizeof(trequete) - sizeof(long);
 	initRand();
 
 	while (1) {
@@ -124,6 +185,7 @@ void forkn(int nbFils, void (*pauBoulot)())
 void traitantSIGINT(int s)
 {
 	msgctl(msgid,IPC_RMID,NULL);
+	semctl(sem_id, 0, IPC_RMID, NULL);
 	exit(0);
 }
 
@@ -132,6 +194,8 @@ main(int argc,char* argv[])
 	int nbVoitures;
 	key_t cle;
 
+	initsem();
+	
 	if (argc-1!=1) {
 		fprintf(stderr,"Appel : %s <nb voitures>\n",argv[0]);
 		exit(2);
@@ -140,7 +204,7 @@ main(int argc,char* argv[])
 	if ((cle = ftok(argv[0],'0')) == -1)
 		erreurFin("Pb ftok");
 	if ((msgid = msgget(cle,IPC_CREAT  | IPC_EXCL | 0600)) == -1)
-		erreurFin("Pb msgget 1");
+		erreurFin("Pb msgget");
 	forkn(nbVoitures,voiture);
 	signal(SIGINT,traitantSIGINT);
 
@@ -153,4 +217,5 @@ main(int argc,char* argv[])
 	}
 
 	msgctl(msgid, IPC_RMID, NULL);
+	semctl(sem_id, 0, IPC_RMID, NULL);
 }
