4a4ded2ee3b2f1c19a6dfcdecb2cc4fcf8cb1eba
Scentle5S <nicolas.bfd@gmail.com>
2015-06-02 17:26:13 +0200

    Commente voiture.c

--- src/voiture.c
+++ src/voiture.c
@@ -1,8 +1,13 @@
+/**
+ * \file voiture.c
+ * \brief Contient les fonctions utiles au fonctionnement et a la representation des voitures.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "project.h"
 #include "voiture.h"
 
+/*! Represente les 12 voies du carrefour. */
 Voie voies[12] = {	{1,{15,16,17,13,8,3},{11,7,10,4,7,8}},
 					{2,{20,21,23,24,-1},{11,7,4,5,-1}},
 					{3,{-1},{-1}},
@@ -16,11 +21,22 @@ Voie voies[12] = {	{1,{15,16,17,13,8,3},{11,7,10,4,7,8}},
 					{11,{0,5,15,20,-1},{8,4,1,2,-1}},
 					{12,{-1},{-1}}	};
 
+/**
+ * \fn void initRand()
+ * \brief Permet de generer de nouveaux nombres aleatoires.
+ * Necessaire pour que les differents processus (les voitures) ne generent pas les memes nombres aleatoires (les memes voies).
+ */
 void initRand()
 {
 	srand(getpid());
 }
 
+/**
+ * \fn void affiche_voiture(Voiture *v)
+ * \brief Affiche les informations d'une voiture.
+ *
+ * \param v Pointeur sur la voiture a afficher.
+ */
 void affiche_voiture(Voiture *v)
 {
 	printf("Numero : %d\n", v->numero);
@@ -35,6 +51,20 @@ void affiche_voiture(Voiture *v)
 	printf("}\n");
 }
 
+/**
+ * \fn void voiture(int numero, int voie)
+ * \brief Fonction realisee par chaque processus (chaque voiture).
+ * Comprend 2 phases :
+ * - Initialisation de la voiture, de deux facons :
+ *  - Avec une voie aleatoire
+ *  - Avec une voie choisie par l'utilisateur
+ * - Parcours de la voie par la voiture.
+ *
+ * \param numero Le numero de la voiture.
+ * \param voie Le numero de la voie. Peut valoir :
+ * - -1 : la voiture prendra une voie dont le numero est genere aleatoirement (1<=voie<=12)
+ * - >0 : la voiture prendra la voie correspondant a ce numero
+ */
 void voiture(int numero, int voie)
 {
 	Voiture v;
@@ -98,6 +128,17 @@ void voiture(int numero, int voie)
 	V(MUTEX);
 }
 
+/**
+ * \fn void maj_position(Voiture *v, int position, int traverse)
+ * \brief Met a jour la position de la voiture dans sa structure.
+ *
+ * \param v Pointeur sur la voiture a mettre a jour.
+ * \param position L'indice du croisement ou se trouve la voiture.
+ * \param traverse L'etat de traversement (directement lie aux informations de traversement). Peut valoir :
+ * - PASTRAVERSE
+ * - TRAVERSE
+ * - ATRAVERSE
+ */
 void maj_position(Voiture *v, int position, int traverse)
 {
 	v->position = position;
